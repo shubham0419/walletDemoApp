45b6f0fc808927414c199f5b9bd0f361952e287f
Your Name <you@example.com>
2024-07-21 18:05:31 +0530

    done

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -1,7 +1,6 @@
 import db from "@repo/db/client";
 import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from "bcrypt";
-import { pages } from "next/dist/build/templates/app-page";
 
 export const authOptions = {
     providers: [
